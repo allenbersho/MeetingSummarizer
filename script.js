ad727073cd457b4a2a136201afa3dd76a969cb2c
allenbersho <allenbersho@gmail.com>
2025-03-06 22:53:25 +0530

    webpage

---
+++ script.js
@@ -0,0 +1,44 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#stars", { 
+    x: 100, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
+
+gsap.to("#moon", { 
+    y: -100, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
+
+gsap.to("#mountback", { 
+    y: -50, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
+
+gsap.to("#mountfront", { 
+    y: 50, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
+
+gsap.to("#text", { 
+    x: -850, 
+    y: 150, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
+
+gsap.to("#btn", { 
+    y: 50, 
+    duration: 3, 
+    ease: "power1.out", 
+    scrollTrigger: { scrub: 1 } 
+});
