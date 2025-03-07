55118952ac46e76ed74a9b47a1127a843ef70790
Ashwin1235 <ashwin.daya12@gmail.com>
2025-03-07 09:44:28 +0530

    Sucess

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("uploadForm").addEventListener("submit", function() {
+    // Show loader when form is submitted
+    document.getElementById("loader").style.display = "block";
+});
